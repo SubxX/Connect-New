0d4bda1835dbc73406121deefdd5955751163efb
Subham Bhattacharya <51792949+SubxX@users.noreply.github.com>
2020-06-14 21:56:46 +0530

    chat-thread-added

--- src/app/modules/chat-window/message/message.component.ts
+++ src/app/modules/chat-window/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
-
+  @Input() type: string;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.type);
   }
 
 }
